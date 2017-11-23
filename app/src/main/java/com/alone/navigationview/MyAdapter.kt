4589e8b99e5758cd15e17f07871be4ae936de4b3
R-NK <right.rcon.f@outlook.com>
2017-11-24 04:02:52 +0900

    履歴画面関係 TODO:overrideの実装

---
+++ app/src/main/java/com/alone/navigationview/MyAdapter.kt
@@ -0,0 +1,25 @@
+package com.alone.navigationview
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by right on 2017/11/23.
+ */
+class MyAdapter : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
