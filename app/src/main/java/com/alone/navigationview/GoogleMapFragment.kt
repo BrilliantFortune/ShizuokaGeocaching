5078388557ecfab54563ff69a6c9afddc5a69307
dysonkiller <venezia4089@gmail.com>
2017-11-29 10:37:53 +0900

    円、マーカー作成 マーカータイトル文字化け有

--- app/src/main/java/com/alone/navigationview/GoogleMapFragment.kt
+++ app/src/main/java/com/alone/navigationview/GoogleMapFragment.kt
@@ -2,18 +2,28 @@ package com.alone.navigationview
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.content.res.AssetManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.PermissionChecker
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import kotlinx.android.synthetic.main.fragment_googlemap.*
 import kotlinx.android.synthetic.main.toolbar.*
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import java.io.*
+import java.util.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import android.support.v7.app.AppCompatActivity
+
+
+
 
 /**
  * Created by right on 2017/11/22.
@@ -34,6 +44,42 @@ class GoogleMapFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(map: GoogleMap?) {
+
+        val su = LatLng(34.724770, 137.717708)
+        //とりあえず静大
+        map?.addMarker(MarkerOptions().position(su).title("Marker in Shizuoka University"))
+        map?.moveCamera(CameraUpdateFactory.newLatLng(su))
+        map?.addCircle(CircleOptions().center(su).radius(200.0).strokeWidth(3.0f).strokeColor(-16711681).fillColor(2013331455))
+
+
+        val assetmanager : AssetManager = this.context.resources.assets
+        val subfiles = assetmanager.list("")
+        //main\\assets内のファイル読み込み
+        try {
+            for (i in subfiles!!.indices) {
+                val inputstream = this.context.resources.assets.open(subfiles[i])
+                val inputstreamreader = InputStreamReader(inputstream)
+                val bufferedreader = BufferedReader(inputstreamreader)
+                var line: String?
+                while (true) {
+                    line = bufferedreader.readLine()
+                    if (line == null)
+                        break
+                    val st = StringTokenizer(line, ",")
+                    val color = Mapcolor(st.nextToken())
+                    val tit = st.nextToken()
+                    val lat = LatLng(st.nextToken().toDouble(), st.nextToken().toDouble())
+                    //マーカーの作成
+                    //map?.addMarker(MarkerOptions().position(lat).title(tit))
+                    //円の作成
+                    map?.addCircle(CircleOptions().center(lat).radius(200.0).strokeWidth(3.0f).strokeColor(color.sc).fillColor(color.fc))
+                }
+                bufferedreader.close()
+            }
+        } catch (e: IOException) {
+            println(e)
+        }
+
         //パーミッションの確認
         if (PermissionChecker.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             //現在地レイヤーを有効化
@@ -42,5 +88,7 @@ class GoogleMapFragment : Fragment(), OnMapReadyCallback {
             //パーミッション有効化のダイアログを表示
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
         }
+
     }
+
 }
